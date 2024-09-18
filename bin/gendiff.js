6b1c13591e1666a1fccdb6c9efa97d66bdf037da
Olga Pavlova <olni.pavlova@gmail.com>
2024-09-18 15:08:48 -0700

    Modify gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,8 @@ const program = new Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
-
