33b599bf997cf45de4c3b1b1e5f45c5622468f77
Olga Pavlova <olni.pavlova@gmail.com>
2024-09-19 13:32:09 -0700

    Add two files for testing, add file for file content parsing, modify gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { fileParse } from './src/fileParser.js';
+import { cwd } from 'node:process';
+
+
+console.log(`Current directory: ${cwd()}`);
 const program = new Command();
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
+  .action((filePath1, filePath2) => {
+    const file1Parsed = fileParse(filePath1);
+    const file2Parsed = fileParse(filePath2);
+    console.log(`first file parsed: ${JSON.stringify(file1Parsed, null, 2)}`);
+    console.log(`second file parsed: ${JSON.stringify(file2Parsed, null, 2)}`);
+
+  })
   .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information');
 
