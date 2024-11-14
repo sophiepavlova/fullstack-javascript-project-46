ecda66ddcea84dc0deb3a795f9a5788bb2254557
sophiepavlova <olni.pavlova@gmail.com>
2024-11-14 13:12:44 -0800

    Fix indents in plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,24 +1,24 @@
 import { formatValue} from '../utils.js';
 
 const plain = (diff) => {
-  const iter = (node, parentPath) => {
-    const lines = node.map(({ key, type, value, value1, value2, children }) => {
-      const currentPath = parentPath ? `${parentPath}.${key}` : key;
-        switch (type) {
-          case 'added':
-              return `Property '${currentPath}' was added with value: ${formatValue(value)}`;
-          case 'removed':
-              return `Property '${currentPath}' was removed`;
-          case 'changed':
-              return `Property '${currentPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`;
-              // case 'unchanged':
-              //     return ``;
-          case 'nested':
-              return iter(children, currentPath);
-          default:
-              return '';
-            }
-        });
+    const iter = (node, parentPath) => {
+      const lines = node.map(({ key, type, value, value1, value2, children }) => {
+        const currentPath = parentPath ? `${parentPath}.${key}` : key;
+          switch (type) {
+            case 'added':
+                return `Property '${currentPath}' was added with value: ${formatValue(value)}`;
+            case 'removed':
+                return `Property '${currentPath}' was removed`;
+            case 'changed':
+                return `Property '${currentPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`;
+                // case 'unchanged':
+                //     return ``;
+            case 'nested':
+                return iter(children, currentPath);
+            default:
+                return '';
+              }
+          });
 
         return lines.filter(line => line).join('\n');
     };
